394522e9dbe1ba832ebe7791e1d5e8c037fd3649
LambCris <157593068+LambCris@users.noreply.github.com>
2024-02-12 18:51:33 +0200

    Add files via upload

---
+++ CW2.js
@@ -0,0 +1,15 @@
+for ( let i=0; i<=10; i++){
+  console.log(i)
+}
+
+for ( let i=0; i<=10; i++){
+  if (i === 7) break;
+  console.log(i);
+}
+
+let i = 0
+while(i <= 20){
+  if(i % 5 !==0) console.log(i);
+  i++
+}
+
