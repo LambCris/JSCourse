3e96481eb20effd4dba1ec152089c53b83d833f6
LambCris <157593068+LambCris@users.noreply.github.com>
2024-02-13 14:20:57 +0200

    Update CW1.js

--- CW2.js
+++ CW2.js
@@ -1,3 +1,21 @@
+// 1 
+
+function getTimeOfDay(time){
+  if( time >= 8 && time <=12 ){
+    return 'сейчас утро';
+  }else if( time >12 && time <=17){
+    return 'сейчас день';
+  }else if (time >17 && time <=24){
+    return 'сейчас вечер';
+  }else{
+    return 'еще ночь';
+  }
+}
+console.log(getTimeOfDay(13))
+
+
+// 2 
+
 for ( let i=0; i<=10; i++){
   console.log(i)
 }
@@ -12,4 +30,27 @@ while(i <= 20){
   if(i % 5 ==0) console.log(i);
   i++
 }
+
+// 3
+
+function formatPhoneNumber(phoneNumber){
+
+  for (let i =0; i <= phoneNumber.length; i++){
+    if( isNaN([i])){
+      return `only numbers are allowed`;
+    }
+  }
+
+  let result = 0;
+
+  let brackets = phoneNumber.slice(0,3);
+  let separator = phoneNumber.slice(3,6);
+  let lastNumbers = phoneNumber.slice(6,`${phoneNumber.length}`);
+  result = `(${brackets})-${separator}-${lastNumbers}`;
+  return result;
+
+}
+console.log(formatPhoneNumber('1234567890'))
+
+
 
