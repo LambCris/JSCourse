8af17d351f2eb27ad6a6c6b304e1bc4dba4c0dfd
LambCris <157593068+LambCris@users.noreply.github.com>
2024-03-17 20:49:59 +0200

    Update script.js

--- HTML-CSS/HW2. project Team24/script.js
+++ HTML-CSS/HW2. project Team24/script.js
@@ -5,3 +5,54 @@ burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   menu.classList.toggle("open");
 });
+
+
+// ------- slider--------------------
+const sliderImages = document.querySelector('.slider_images');
+const images = document.querySelectorAll('.slider_images .slider_img');
+const prevBtn = document.querySelector('.left');
+const nextBtn = document.querySelector('.right');
+const dots = document.querySelectorAll('.dots_container .wrapper span')
+
+let index = 0;
+let lengthItems = images.length -1;
+
+
+nextBtn.addEventListener('click', function(){
+  if (index+1 > lengthItems){
+    return;
+  }else{
+    index ++;
+  }
+  showSlide();
+  let activeDot = document.querySelector('.slider_dots .dots_container .wrapper span.checked');
+  activeDot.classList.remove('checked')
+  dots[index].classList.add('checked')
+})
+
+function showSlide(){
+  let checkLeft = images[index].offsetLeft;
+  sliderImages.style.left = -checkLeft +'px';
+}
+
+prevBtn.addEventListener('click', function(){
+  if (index-1 <0){
+    return;
+  }else{
+    index --;
+  }
+  showSlide();
+  let activeDot = document.querySelector('.slider_dots .dots_container .wrapper span.checked');
+  activeDot.classList.remove('checked')
+  dots[index].classList.add('checked')
+})
+
+dots.forEach( (dot,ind)=>{
+  dot.addEventListener('click', function(){
+    index = ind;
+    showSlide();
+    let activeDot = document.querySelector('.slider_dots .dots_container .wrapper span.checked');
+    activeDot.classList.remove('checked')
+    dots[index].classList.add('checked')
+  })
+})
